b8f5caa393e4116e947eb6b560465853bd34f456
Ayush9049 <ayushgawali11@gmail.com>
2026-06-06 14:16:32 +0530

    save projects to database

--- backend/app/api/projects.py
+++ backend/app/api/projects.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
-from app.schemas.project import ProjectCreate
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 import secrets
+
+from app.schemas.project import ProjectCreate
+from app.models.project import Project
+from app.core.database import get_db
 
 router = APIRouter(
     prefix="/api/projects",
@@ -9,10 +13,24 @@ router = APIRouter(
 
 
 @router.post("")
-def create_project(project: ProjectCreate):
+def create_project(
+    project: ProjectCreate,
+    db: Session = Depends(get_db)
+):
     api_key = f"pk_live_{secrets.token_hex(16)}"
 
+    new_project = Project(
+        name=project.name,
+        api_key=api_key
+    )
+
+    db.add(new_project)
+    db.commit()
+    db.refresh(new_project)
+
     return {
-        "name": project.name,
-        "api_key": api_key
+        "id": new_project.id,
+        "name": new_project.name,
+        "api_key": new_project.api_key,
+        "created_at": new_project.created_at
     }
