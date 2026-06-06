b0c6a9e6381ff1a3c48b50069ba9ea95bb9c20c7
Ayush9049 <ayushgawali11@gmail.com>
2026-06-06 13:15:49 +0530

    update projects.py

--- backend/app/api/projects.py
+++ backend/app/api/projects.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
 from app.schemas.project import ProjectCreate
+import secrets
 
-router = APIRouter(prefix="/api/projects", tags=["projects"])
+router = APIRouter(
+    prefix="/api/projects",
+    tags=["projects"]
+)
 
 
 @router.post("")
 def create_project(project: ProjectCreate):
+    api_key = f"pk_live_{secrets.token_hex(16)}"
+
     return {
-        "name": project.name
+        "name": project.name,
+        "api_key": api_key
     }
