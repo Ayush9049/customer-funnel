cc9fb444b3607d05950bbaf72153d0ca78bea56c
Ayush9049 <ayushgawali11@gmail.com>
2026-06-16 13:02:11 +0530

    add dashboard routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,15 @@
-import LoginPage from './pages/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginPage from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
